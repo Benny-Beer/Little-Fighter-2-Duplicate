1d84b9e7e309a4df6b9a9e6c74d6d034899101d8
OmerShim <omershim97@gmail.com>
2025-07-14 09:29:20 +0300

    nothing happened

--- src/Objects/Weapons/Rock.cpp
+++ src/Objects/Weapons/Rock.cpp
@@ -60,18 +60,7 @@ void Rock::update(float dt)
     apllySprite();
 }
 
-//need to get the direction and groundY from the player
-void Rock::throwRock(int direction, float groundY, float speed)
-{
-    m_isFlying = true;
-    m_groundY = groundY;
-    
-    // מהירות התחלתית:
-    m_velocity.x = direction * speed * 0.5 ;
-    m_velocity.y = -400.f;  // לזרוק קצת למעלה (שלילי כי Y יורד למטה)
-	setAnimation(AnimationManager::getAnimation("rock_flying", getTexture()));
 
-// }
 
 bool Rock::m_registered = Factory<PickableObject>::registerIt("r", [](const sf::Vector2f& pos, const std::string& name) {
     return std::make_unique<Rock>(pos, name);
